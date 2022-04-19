21cab496dd30e3b8ca370e59108a1fd37884fc96
salinsalins <sanin_andrey@mail.ru>
2022-04-19 10:40:37 +0700

    commit

--- log_exception.py
+++ log_exception.py
@@ -3,9 +3,13 @@ import logging
 import sys
 
 
-def log_exception(logger, message=None, *args, level=logging.ERROR, **kwargs):
+def exception_shot_info():
     ex_type, ex_value, traceback = sys.exc_info()
-    tail = ' %s: %s' % (ex_type.__name__, ex_value)
+    return ' %s: %s' % (ex_type.__name__, ex_value)
+
+
+def log_exception(logger, message=None, *args, level=logging.ERROR, **kwargs):
+    tail = exception_shot_info()
     if message is None:
         message = 'Exception'
     message += tail
@@ -26,8 +30,7 @@ def log_exception(logger, message=None, *args, level=logging.ERROR, **kwargs):
         logger.debug('Exception: ', exc_info=True)
         return message
     except:
-        ex_type, ex_value, traceback = sys.exc_info()
-        tail = ' %s: %s' % (ex_type.__name__, ex_value)
+        tail = exception_shot_info()
         print('Unexpected exception in log_exception ', tail)
         print('Previous exception:', message)
         return message
