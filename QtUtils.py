c2b547302089db0af82a71bfab6d3615f73a87f8
salinsalins <sanin_andrey@mail.ru>
2022-11-15 15:22:57 +0700

    commit

--- QtUtils.py
+++ QtUtils.py
@@ -1,5 +1,6 @@
 import json
 import logging
+import time
 
 from PyQt5 import QtWidgets
 from PyQt5.QtCore import QSize, QPoint
@@ -129,10 +130,12 @@ class WidgetLogHandler(logging.Handler):
         logging.Handler.__init__(self)
         self.widget = widget
         self.limit = limit
+        self.widget.time = 0.0
 
     def emit(self, record):
         log_entry = self.format(record)
-        if self.limin > 0:
+        if self.limit > 0:
             log_entry = log_entry[:self.limit]
         if self.widget is not None:
+            self.widget.time = time.time()
             self.widget.setText(log_entry)
