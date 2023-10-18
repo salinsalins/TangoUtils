ba0fcf46590cc7323f96e43b233961c27083063f
salinsalins <sanin_andrey@mail.ru>
2023-10-18 13:59:47 +0700

    smooth added

---
+++ smooth.py
@@ -0,0 +1,13 @@
+def smooth(self, array, n):
+    m = int(len(array) / n)
+    k = int(len(array) % n)
+    if m > 0:
+        sa = array[:m * n].reshape(m, n).mean(axis=1)
+        if k > 0:
+            sb = numpy.zeros(m + 1)
+            sb[:-1] = sa
+            sb[-1] = sa[-k:].mean()
+            return sb
+    else:
+        sa = array.mean()
+    return sa
