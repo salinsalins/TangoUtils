4fcbbfd600e1ed933d208e556863a5992735b3ae
salinsalins <sanin_andrey@mail.ru>
2024-11-25 09:19:36 +0700

    minpr corrections

--- TangoServerPrototype.py
+++ TangoServerPrototype.py
@@ -432,12 +432,12 @@ class TangoServerPrototype(Device):
         # self.tango_logging = True
 
     def set_running(self, msg='I/O OK'):
-        self.logger.info(msg)
+        self.logger.info(msg, stacklevel=2)
         self.set_state(DevState.RUNNING)
         self.set_status(msg)
 
     def set_fault(self, msg='Error during I/O'):
-        self.logger.error(msg)
+        self.logger.error(msg, stacklevel=2)
         self.set_state(DevState.FAULT)
         self.set_status(msg)
 
