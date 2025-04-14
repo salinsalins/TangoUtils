7446696325927f96d082a35707207261c2b75e0c
salinsalins <sanin_andrey@mail.ru>
2025-04-14 09:40:01 +0700

    import Utils

---
+++ Utils.py
@@ -0,0 +1,4 @@
+import os, sys
+
+if os.path.realpath('../TangoUtils') not in sys.path: sys.path.append(os.path.realpath('../TangoUtils'))
+if os.path.realpath('../IT6900') not in sys.path: sys.path.append(os.path.realpath('../IT6900'))
