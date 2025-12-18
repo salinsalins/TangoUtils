ecc2cbc0145fd910c685370a88b3844643f790c3
sanin <sanin_andrey@mail.ru>
2025-12-19 02:03:37 +0700

    lauda standalone changed

--- Utils.py
+++ Utils.py
@@ -1,4 +1,11 @@
 import os, sys
 
-if os.path.realpath('../TangoUtils') not in sys.path: sys.path.append(os.path.realpath('../TangoUtils'))
-if os.path.realpath('../IT6900') not in sys.path: sys.path.append(os.path.realpath('../IT6900'))
+def append_utils_folder_to_path(path='TangoUtils'):
+    real = os.path.realpath('../' + path)
+    if not os.path.exists(real):
+        raise ImportError('Can not find %s folder' % path)
+    if real not in sys.path:
+        sys.path.append(real)
+
+append_utils_folder_to_path('TangoUtils')
+append_utils_folder_to_path('IT6900')
